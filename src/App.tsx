a8d63e1a0f818f637eb42ca9831a54669078a06a
Dima <dimaberry1@gmail.com>
2023-02-04 20:17:43 +0100

    init: start rs-clone task

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="App bg-red-500 w-10 h-10"></div>;
+}
+
+export default App;
