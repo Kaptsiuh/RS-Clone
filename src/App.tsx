5f196bba263ca5e20f3b20aa630ae98de35f74bd
Dima <dimaberry1@gmail.com>
2023-02-05 19:07:29 +0100

    feat: add home page structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { FC } from "react";
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+const App: FC = () => {
+  // const [count, setCount] = useState(0);
 
-  return <div className="App bg-red-500 w-10 h-10"></div>;
-}
+  return (
+    <div className="grid grid-cols-8">
+      <div className="bg-black text-white col-start-1 col-end-3 min-h-screen">
+        Nurlan
+      </div>
+      <div className="bg-red-500 col-start-3 col-end-9">
+        <div className="w-full h-20 bg-blue-300">Dima</div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
