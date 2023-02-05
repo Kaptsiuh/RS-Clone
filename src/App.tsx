d1828c5419f375d3594e3c3c58d49078d914bde6
Dima <dimaberry1@gmail.com>
2023-02-05 23:32:15 +0100

    feat: add the structure of the articl

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,30 @@ const App: FC = () => {
         Nurlan
       </div>
       <div className="bg-red-500 col-start-3 col-end-9">
-        <div className="w-full h-20 bg-blue-300">Dima</div>
+        <div className="w-full h-20 bg-gray-200 shadow-2xl flex items-center">
+          <input
+            type="text"
+            placeholder="What are you working on?"
+            className="text-gray-600 bg-gray-200 w-5/6 ml-10"
+          />
+          <span className="w-1/12">ico</span>
+          <span className="w-1/12">ico</span>
+          <span className="w-1/12">ico</span>
+          <span className="w-1/12">timer</span>
+          <button className="w-1/12">play</button>
+          <span className="w-1/12">toggle</span>
+        </div>
+        <div className="w-full h-20 bg-gray-100 flex items-center justify-around">
+          <h2>period</h2>
+          <h2>period total</h2>
+          <h2>period time</h2>
+          <button>left</button>
+          <div>schedule</div>
+          <button>right</button>
+        </div>
+        <div className="w-full h-20 bg-gray-100 flex items-center justify-center">
+          <div>progress bar</div>
+        </div>
       </div>
     </div>
   );
